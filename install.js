ab3dcc6eff772b4621350d59afa8e60f1cd3772f
Jack Huynh <truonghuynhxuan1995@gmail.com>
2024-10-22 14:54:46 +0700

    Update install.js

--- install.js
+++ install.js
@@ -1,5 +1,26 @@
 var fs = require('fs');
+var path = require('path');
+var unzipper = require('unzipper');
 var util = require('util');
+
+const zipFilePath = path.join(__dirname, 'lab.zip'); // Path to the lab.zip file
+const outputDir = path.join(__dirname, 'lab_output'); // Directory to extract files into
+
+// Create the output directory if it doesn't exist
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+}
+
+// Unzip the file
+fs.createReadStream(zipFilePath)
+    .pipe(unzipper.Extract({ path: outputDir }))
+    .on('close', () => {
+        console.log('Unzipping completed successfully.');
+    })
+    .on('error', (err) => {
+        console.error('An error occurred while unzipping:', err);
+    });
+
 var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 var log_stdout = process.stdout;
 
